b996b634c3adc5cfda3db436600e895abb9d7b6c
Saroj Sah <saroj.sah@s2022.tu-chemnitz.de>
2024-04-08 00:07:28 +0200

    configured tailwindcss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,9 @@ import "./App.css";
 function App() {
   return (
     <div className="app">
-      <h1>Rate My Uni</h1>
+      <h1 className="text-3xl">Rate My Uni</h1>
+
+      <button className="btn btn-primary">Hello World</button>
     </div>
   );
 }
