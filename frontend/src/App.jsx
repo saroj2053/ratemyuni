be657497a025d89bffe97421d901cd653115573d
Saroj Sah <saroj.sah@s2022.tu-chemnitz.de>
2024-04-08 14:22:26 +0200

    removed daisyui  dependency

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,7 @@
-import "./App.css";
-
 function App() {
   return (
-    <div className="app">
-      <h1 className="text-3xl">Rate My Uni</h1>
-
-      <button className="btn btn-primary">Hello World</button>
+    <div className="h-screen flex justify-center items-center">
+      <h1 className="text-3xl">Welcome to Rate My Uni App 🚀</h1>
     </div>
   );
 }
