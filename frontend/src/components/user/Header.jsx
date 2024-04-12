a01452fed9e4bf16db9f7f42760c5efb30adde7b
Saroj Sah <saroj.sah@s2022.tu-chemnitz.de>
2024-04-12 08:17:19 +0200

    added logout functionality

--- frontend/src/components/user/Header.jsx
+++ frontend/src/components/user/Header.jsx
@@ -3,14 +3,16 @@ import logo from "../../assets/logo.svg";
 import { FaSearch } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 import useUserContext from "../../context/UserContext";
+import useLogout from "../../hooks/useLogout";
 
 const Header = () => {
   const navigate = useNavigate();
   const { user } = useUserContext();
+  const { loading, logout } = useLogout();
   return (
     <header className="bg-white border-2 border-b-grey-light-2 font-grotesk w-full">
       <div className="max-w-[90%] mx-auto py-6 flex justify-between items-center">
-        <div className="flex gap-1 items-center w-[50%] md:w-[30%]">
+        <div className="flex gap-1 items-center w-[40%] md:w-[30%]">
           <img
             src={logo}
             className="w-14 h-14 "
@@ -19,7 +21,7 @@ const Header = () => {
           <h1 className="text-2xl font-bold text-slate-700">Rate My Uni</h1>
         </div>
 
-        <div className="relative w-[35%] hidden md:block">
+        <div className="relative w-[30%] hidden lg:block">
           <form action="#">
             <input
               className="bg-grey-light-2 py-3 pl-8 pr-16 rounded-full w-full focus:outline-none font-grotesk"
@@ -36,11 +38,17 @@ const Header = () => {
           </form>
         </div>
 
-        <div className="w-1/5 flex justify-end">
+        <div className="w-[30%] flex justify-end">
           {user ? (
-            <ul>
+            <ul className="flex gap-10 justify-between items-center">
               <li className="cursor-pointer">
                 <img className="w-14 h-14" src={user.profileAvatar} alt="" />
+              </li>
+              <li
+                className="text-base font-semibold py-2 px-4 rounded-md cursor-pointer border-2 border-primary-dark text-grey-dark-1 transition ease-in delay-100 hover:bg-primary-dark hover:text-white"
+                onClick={logout}
+              >
+                Logout
               </li>
             </ul>
           ) : (
